8db55487471fc89ad6cfbeab0751907197b91189
código4edutech <ibt.centralcontato@gmail.com>
2025-01-25 20:15:14 -0300

    Separando os arquivos em HTML, CSS e JavaScript

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Adiciona um efeito suave de zoom quando a galeria é clicada (opcional)
+document.querySelectorAll('.gallery-item').forEach(item => {
+  item.addEventListener('click', () => {
+     alert("Imagem clicada!");
+  });
+});
